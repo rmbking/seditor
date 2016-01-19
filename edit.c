dea3dacd924f864aa0212a3ff96482b7ab83d1c4
rmbking <rmbking@sina.cn>
2016-01-19 19:51:12 +0800

    First try to modify the file and it work.
    Signed-off-by: rmbking <rmbking@sina.cn>

--- edit.c
+++ edit.c
@@ -1,6 +1,30 @@
 #include "main.h"
+#include "kbhit.h"
 #include "control.h"
+#include "view.h"
+void insert_word(char c)
+{
+	int k;
+	int line = file.cur_line;
+	int index = file.cur_index;
+	k = file.line[line].line_end + 1;
+	if(index + 1 <= lengthof(file.line[line]))		
+	{
+		while(--k >= index)
+			file.line[line].character[k+1] = file.line[line].character[k];
+		file.line[line].character[index] = c;	
+		CursorRight(1);
+		file.line[file.cur_line].line_end++;
+	}
+		
+}
 int edit()
 {
+	char word;
+	while((word = kb_input()) != 'q')	
+	{
+		insert_word(word);
+		display(file.start_line);
+	}
 	return EXIT;
 }
